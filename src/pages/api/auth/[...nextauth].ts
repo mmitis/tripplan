048171b336da46c7d53dd08423393e57c358c418
Marek Mitis <mmitis@Mareks-MacBook-Air.local>
2022-12-30 14:55:44 +0100

    up deps

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -30,14 +30,14 @@ export const nextAuthOptions = {
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
 
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
-          return user;
+          return Promise.resolve(user);
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
-          return null;
+          return Promise.resolve(null);
 
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
